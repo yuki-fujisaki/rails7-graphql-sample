3f9145dc207927e00ad03034cabd10b6b0637351
yuki-fujisaki <yuki.f.bizz@gmail.com>
2023-08-04 19:49:04 +0900

    add: db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,8 @@
 
 ActiveRecord::Schema[7.0].define(version: 2023_08_04_104631) do
   create_table "users", charset: "utf8mb4", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
