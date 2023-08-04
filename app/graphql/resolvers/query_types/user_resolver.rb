2ec49e8099a4bb881377c3e0b684a5a739801eea
yuki-fujisaki <yuki.f.bizz@gmail.com>
2023-08-04 20:03:41 +0900

    add: module Resolvers::QueryTypesで囲わないとダメ

--- app/graphql/resolvers/query_types/user_resolver.rb
+++ app/graphql/resolvers/query_types/user_resolver.rb
@@ -1,8 +1,10 @@
-class UserResolver < GraphQL::Schema::Resolver
-    type Types::UserType, null: false
-    argument :id, ID, required: false
+module Resolvers::QueryTypes
+    class UserResolver < GraphQL::Schema::Resolver
+        type Types::UserType, null: false
+        argument :id, ID, required: false
 
-    def resolve(id:)
-      User.find(id)
+        def resolve(id:)
+        User.find(id)
+        end
     end
 end
